841adec27aa4c27c7b1371f35933b5d0c572d894
Connor Nicholson <connorjnicholson1@gmail.com>
2019-11-27 10:03:06 -0500

    Possible fix for Heroku not deploying app

---
+++ build/precache-manifest.36f67803bee4beeea9dd48b5ae97c916.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfa99cedbe9c0c6e9a9e8dc77441b626",
+    "url": "/LennyDickey/counselor/index.html"
+  },
+  {
+    "revision": "d5edb2211fdc02b7fd46",
+    "url": "/LennyDickey/counselor/static/css/2.21da5c49.chunk.css"
+  },
+  {
+    "revision": "7a1b068721452ff0f088",
+    "url": "/LennyDickey/counselor/static/css/main.fe4fa637.chunk.css"
+  },
+  {
+    "revision": "d5edb2211fdc02b7fd46",
+    "url": "/LennyDickey/counselor/static/js/2.e83fd0eb.chunk.js"
+  },
+  {
+    "revision": "7a1b068721452ff0f088",
+    "url": "/LennyDickey/counselor/static/js/main.0278d59b.chunk.js"
+  },
+  {
+    "revision": "54e369e79987cadbe173",
+    "url": "/LennyDickey/counselor/static/js/runtime-main.056c03c9.js"
+  }
+]);
